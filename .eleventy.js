f0dd59e0839297ae489c75eb21ccaabc235696ff
sebastienmariette74 <61980470+sebastienmariette74@users.noreply.github.com>
2022-07-13 17:15:11 +0200

    add prices

--- .eleventy.js
+++ .eleventy.js
@@ -13,7 +13,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/assets");
     eleventyConfig.addPassthroughCopy("src/css");
     eleventyConfig.addPassthroughCopy("src/js");
-    // eleventyConfig.addPassthroughCopy("src/prices");
+    eleventyConfig.addPassthroughCopy("src/prices");
     eleventyConfig.addPassthroughCopy("prices");
     eleventyConfig.addWatchTarget("src/css");
 
