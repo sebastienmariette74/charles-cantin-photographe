d8ae621f574348e1764e992c02c6be219feee43c
sebastienmariette74 <61980470+sebastienmariette74@users.noreply.github.com>
2022-07-21 11:51:38 +0200

    css header

--- src/js/script.js
+++ src/js/script.js
@@ -1,140 +1,3 @@
-// let select = document.getElementById('select');
-// let displayImages = document.getElementById('displayImages');
-// console.log(select);
-// console.log('script ok');
-
-// let gallery = [
-//     {
-//         name : "wedding01",
-//         category : "wedding"
-//     },
-//     {
-//         name : "wedding02",
-//         category : "wedding"
-//     },
-//     {
-//         name : "wedding03",
-//         category : "wedding"
-//     },
-//     {
-//         name : "wedding04",
-//         category : "wedding"
-//     },
-//     {
-//         name : "wedding05",
-//         category : "wedding"
-//     },
-//     {
-//         name : "wedding06",
-//         category : "wedding"
-//     },
-//     {
-//         name : "wedding07",
-//         category : "wedding"
-//     },
-//     {
-//         name : "wedding08",
-//         category : "wedding"
-//     },
-//     {
-//         name : "wedding09",
-//         category : "wedding"
-//     },
-//     {
-//         name : "wedding10",
-//         category : "wedding"
-//     },
-//     {
-//         name : "wedding11",
-//         category : "wedding"
-//     },
-//     {
-//         name : "wedding12",
-//         category : "wedding"
-//     },
-//     {
-//         name : "pregnant01",
-//         category : "pregnant"
-//     },
-//     {
-//         name : "pregnant02",
-//         category : "pregnant"
-//     },
-//     {
-//         name : "pregnant03",
-//         category : "pregnant"
-//     },
-//     {
-//         name : "pregnant04",
-//         category : "pregnant"
-//     },
-//     {
-//         name : "pregnant05",
-//         category : "pregnant"
-//     },
-//     {
-//         name : "pregnant06",
-//         category : "pregnant"
-//     },
-//     {
-//         name : "pregnant07",
-//         category : "pregnant"
-//     },
-//     {
-//         name : "pregnant08",
-//         category : "pregnant"
-//     },
-//     {
-//         name : "pregnant09",
-//         category : "pregnant"
-//     },
-//     {
-//         name : "pregnant10",
-//         category : "pregnant"
-//     },
-//     {
-//         name : "pregnant11",
-//         category : "pregnant"
-//     },
-//     {
-//         name : "pregnant12",
-//         category : "pregnant"
-//     }
-// ]
-
-// let display = [];
-
-// let disp = () => {
-//     for (let photo in display){
-//         console.log(photo);
-//         displayImages.textContent = `<img src="/assets/images/${photo}.jpg"`
-//     }
-// }
-
-// let target = "";
-
-// // for (let photo of gallery){
-// //     let photoName = `<img src="/assets/images/${photo.name}.jpg" width="100px"></img>` ;
-// //     displayImages.innerHTML += photoName;
-// // }
-
-// function displayPhotos () {
-//     select.addEventListener('change', (event)=>{
-//         displayImages.innerHTML = "";
-//         display = [];
-//         target = event.target.value;
-//         for (let photo of gallery){
-//             if (photo.category === target && target != ""){
-//                 display.push(photo.name);
-//                 let photoName = `<img src="/assets/images/${photo.name}.jpg" width="100px"></img>` ;
-//                 displayImages.innerHTML += photoName;                
-//             } 
-//         }
-//     })
-// }
-
-// displayPhotos();
-
 /* ____________________ MENU BURGER ___________________*/
 
 let main = document.getElementById('main');
@@ -142,7 +5,20 @@ let burger = document.getElementById('burger');
 
 let toggleBurger = false;
 
-main.style.display = "none";
+let changeWidth = () => {
+    return window.innerWidth;
+};
+
+let size = changeWidth();
+if (size < 576){
+    main.style.display = "none";
+    burger.style.display = "block";
+} else {
+    main.style.display = "block";
+    burger.style.display = "none";
+}
+
+// main.style.display = "none";
 // main.style.top = "-170px";
 
 burger.addEventListener("click", () => {
@@ -161,7 +37,8 @@ burger.addEventListener("click", () => {
 });
 
 window.addEventListener('click', (event)=>{
-    if (event.target != burger){
+    const size = changeWidth();
+    if (event.target != burger && size < 576){
         main.style.display = "none";
         toggleBurger = false;
     }
@@ -181,15 +58,15 @@ window.addEventListener('click', (event)=>{
 /* ___________________________________________________ */
 
 
-let changeWidth = () => {
-    return window.innerWidth;
-};
+
   
-window.addEventListener('resize', () => {
+window.addEventListener('resize', () => {    
     const size = changeWidth();
     if (size > 576){
-        main.style.display = "block";
-    } else {
+        main.style.display = "flex";
+    } 
+    else {
         main.style.display = "none";
+        burger.style.display = "block";
     }
 });
